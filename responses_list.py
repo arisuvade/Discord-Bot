31ea787c0a1546cf296969750b89f6be1fa8c1f9
Aries Dave Bautista <110705264+arisuvade@users.noreply.github.com>
2022-10-02 17:27:34 +0800

    add lists

---
+++ responses_list.py
@@ -0,0 +1,46 @@
+greetings = {
+    "hello": "Hi there!",
+    "hi": "Hello there!",
+    "hey": "Hey there!",
+    "how are you?": "I'm fine, how about you?",
+    "i'm fine, too": "Good to hear that.",
+}
+
+quotes = [
+    "I'm sick of following my dreams, man. I'm just going to \
+ask where they're going and hook up with 'em later.",
+    "My mother always used to say: The older you get, the better \
+you get, unless you're a banana.",
+    "Before you criticize someone, you should walk a mile in their \
+shoes. That way when you criticize them, you are a mile \
+away from them and you have their shoes.",
+    "Clothes make the man. Naked people have little or no influence \
+in society.",
+    "Before you marry a person, you should first make them use a \
+computer with slow Internet to see who they really are.",
+    "Insomnia sharpens your math skills because you spend all night \
+calculating how much sleep you'll get if you're able to fall \
+asleep right now.",
+    "Breaking up is like knocking over a Coke machine. You can't do \
+it in one push; you got to rock it back and forth a few times, \
+and then it goes over.",
+    "There is no sunrise so beautiful that it is worth waking me \
+up to see it.",
+    "Common sense is like deodorant. The people who need it most \
+never use it.",
+    "If I'm not back in five minutes, just wait longer.",
+]
+
+jokes = [
+    "How do you open a banana? With a mon-key.",
+    "Where does Batman go to the bathroom? The batroom.",
+    "What did the left eye say to the right eye? Between \
+you and me, something smells.",
+    "What did the mama tomato say to the baby tomato? Catch up!",
+    "What do you call a fake noodle? An impasta.",
+    "How did the pig get to the hogspital? In a hambulance.",
+    "I'm so good at sleeping I can do it with my eyes closed!",
+    "How does an octopus go into battle? Well-armed.",
+    "Where do young trees go to learn? Elementree school."
+    "Why was six afraid of seven? Because 7-8-9.",
+]
